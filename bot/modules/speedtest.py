f6293648fb6fe0e993495e39c77e70a22745084c
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-04-20 07:46:33 +0800

    Add files via upload

---
+++ bot/modules/speedtest.py
@@ -0,0 +1,52 @@
+from speedtest import Speedtest
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot import dispatcher, AUTHORIZED_CHATS
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram.ext import CallbackContext, Filters, run_async, CommandHandler
+
+
+@run_async
+def speedtest(update, context):
+    message = update.effective_message
+    ed_msg = message.reply_text("🔁 Running Speed Test")
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    path = (result['share'])
+    string_speed = f'''
+<b>✅ Done</b>
+
+<b>📶 Server</b>
+<b>📃 Name:</b> <code>{result['server']['name']}</code>
+<b>🌍 Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+    
+<b>SpeedTest Results</b>
+<b>📤 :</b> <code>{speed_convert(result['upload'] / 8)}</code>
+<b>📥 :</b>  <code>{speed_convert(result['download'] / 8)}</code>
+<b>📶 :</b> <code>{result['ping']} ms</code>
+'''
+    ed_msg.delete()
+    try:
+        update.effective_message.reply_photo(path, string_speed, parse_mode=ParseMode.HTML)
+    except:
+        update.effective_message.reply_text(string_speed, parse_mode=ParseMode.HTML)
+
+def speed_convert(size):
+    """Hi human, you can't read bytes?"""
+    power = 2 ** 10
+    zero = 0
+    units = {0: "", 1: "Kb/s", 2: "MB/s", 3: "Gb/s", 4: "Tb/s"}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
+
+
+SPEED_HANDLER = CommandHandler(BotCommands.SpeedCommand, speedtest, 
+                                                  filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
+
+dispatcher.add_handler(SPEED_HANDLER)
